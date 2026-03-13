5edc9de9188daedf04c74f865b7d78ea143c8072
JadhavAnsh <jadhavansh10@gmail.com>
2026-03-14 01:30:29 +0530

    refactor: remove unused font imports and simplify layout class names delete: remove obsolete add_notion_postman.py script

--- frontend/components/mode-toggle.tsx
+++ frontend/components/mode-toggle.tsx
@@ -1,4 +1,3 @@
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
@@ -15,7 +14,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger>
         <Button variant="ghost" size="icon" className="hover:bg-black/5 dark:hover:bg-white/5 cursor-pointer">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
